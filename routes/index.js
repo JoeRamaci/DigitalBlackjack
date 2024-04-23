15a92bba70ab7be1d7bd939aee5f11efa6ca10cd
NoahClive <Noahclive0717@gmail.com>
2024-04-23 11:04:24 -0400

    Modified routes to fix login.

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,28 @@
 
 var express = require('express');
 var router = express.Router();
-var path = require('path');
-var passport = require('passport');
-var bcrypt = require('bcryptjs');
 
-/* Database funcitonality */
+var path = require('path');
 var env = require('dotenv').config();
+
 const Client = require('pg').Client;
 const client = new Client({
 connectionString: process.env.DATABASE_URL
 }); 
 client.connect(); // connect to the DATABASE_URL
+
+var passport = require('passport');
+var bcrypt = require('bcryptjs');
+
+router.get('/logout', function(req, res, next){
+  req.logout(function(err){
+    if (err) {
+      console.log("unable to logout:", err);
+      return next(err);
+    }
+  });
+  res.redirect('/login')
+});
 
 /* login page 
 * localhost:3000/login */
@@ -26,6 +37,16 @@ router.get('/', function(req, res, next) {
 router.get('/createAccount', function(req, res, next) {
   res.sendFile(path.join(__dirname, '..', 'public', 'createAccount.html'));
 });
+
+
+ /* login page
+ * localhost:3000/ */
+router.post('/',
+passport.authenticate('local', {failureRedirect: 'login?message=Incorrect+credentials', failureFlash:true }),
+function(req, res, next){
+  console.log
+  res.redirect('/game');
+})
 
 /* Game page
 * localhost:3000/game */
@@ -79,5 +100,52 @@ router.get('/losersOut',function(req, res, next){
   });
 });
 
+router.get('/createAccount', function(req, res, next){
+res.sendFile(path.join(__dirname,'..','public','createAccount.html'));
+});
+
+/* create account page
+ * localhost:3000/createAccount */
+ router.post('/createAccount', 
+ function(req, res, next) {
+  if (req.body.username == null) {
+    console.log("USERNAME EMPTY");
+    res.redirect('/DigitalBlackjack/createAccount?message=Please+enter+a+username');
+  } else if (req.body.password == null) {
+    console.log("PASSWORD EMPTY");
+    res.redirect('/DigitalBlackjack/createAccount?message=Please+enter+a+password');
+  } else {
+    var salt = bcrypt.genSaltSync(10);
+    var encryptPassword = bcrypt.hashSync(req.body.password, salt);
+    client.query('SELECT * FROM Blackjack_user WHERE username = $1', [req.body.username], function(err, result) {
+      if (err) {
+        console.log("unable to query SELECT");
+        next(err);
+      }
+      // if user already existing
+      if (result.rows.length > 0) {
+        console.log("user exists");
+        res.redirect('/DigitalBlackjack/createAccount?message=User+exists')
+      } else {
+        client.query('INSERT INTO Blackjack_user (username, password, win_count, loss_count) VALUES($1, $2, $3, $4)', [req.body.username, encryptPassword, 0, 0], function(err, result) {
+          if (err) {
+            console.log("unable to query INSERT");
+            next(err);
+          }
+          console.log("ENCRYPTED PASS: " + encryptPassword) //DEBUG
+          console.log("New user created");
+          res.redirect('/DigitalBlackjack/createAccount?message=New+user+created.+Try+logging+in!');
+        });
+      }
+    });
+  }
+ });
+
+/* game page
+ * localhost:3000/game
+ * should post a user's win or loss when it occurs */
+router.post('/game', function(req, res, next) {
+  client.query();
+});
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,78 +3,9 @@
 
 var express = require('express');
 var router = express.Router();
-var path = require('path');
-var passport = require('passport');
-var bcrypt = require('bcryptjs');
 
-/* Database funcitonality */
-var env = require('dotenv').config();
-const Client = require('pg').Client;
-const client = new Client({
-  connectionString: process.env.DATABASE_URL
-}); 
-client.connect(); // connect to the DATABASE_URL
-
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-
- /* login page
- * localhost:3000/ */
- router.post('/', 
- passport.authenticate('local', { failureRedirect: '?message=Incorrect+credentials', failureFlash:true }),
- function(req, res, next) {
-    res.redirect('/game');
-});
-
-router.get('/createAccount', function(req, res, next){
-res.sendFile(path.join(__dirname,'..','public','createAccount.html'));
-});
-
-/* create account page
- * localhost:3000/createAccount */
- router.post('/createAccount', function(req, res, next) {
-  if (req.body.username == null) {
-    console.log("USERNAME EMPTY");
-    res.redirect('/createAccount?message=Please+enter+a+username');
-  } else if (req.body.password == null) {
-    console.log("PASSWORD EMPTY");
-    res.redirect('/createAccount?message=Please+enter+a+password');
-  } else {
-    var salt = bcrypt.genSaltSync(10);
-    var encryptPassword = bcrypt.hashSync(req.body.password, salt);
-    client.query('SELECT * FROM blackjack_user WHERE username = $1', [req.body.username], function(err, result) {
-      if (err) {
-        console.log("unable to query SELECT");
-        next(err);
-      }
-      // if user already existing
-      if (result.rows.length > 0) {
-        console.log("user exists");
-        res.redirect('/createAccount?message=User+exists')
-      } else {
-        client.query('INSERT INTO blackjack_user (username, password, win_count, loss_count) VALUES($1, $2, $3, $4)', [req.body.username, encryptPassword, 0, 0], function(err, result) {
-          if (err) {
-            console.log("unable to query INSERT");
-            next(err);
-          }
-          console.log("ENCRYPTED PASS: " + encryptPassword) //DEBUG
-          console.log("New user created");
-          res.redirect('/?message=New+user+created.+Try+logging+in!');
-        });
-      }
-    });
-  }
- });
-
-/* game page
- * localhost:3000/game
- * should post a user's win or loss when it occurs */
-router.post('/game', function(req, res, next) {
-  client.query();
+router.get('/', function(req, res, next){
+  res.send('respond with a resource')
 });
 
 module.exports = router;
